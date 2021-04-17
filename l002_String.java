210fe80cf764a86f6e7ada9b3ced998f04a02e77
prachi2399 <prachi2399@gmail.com>
2021-04-17 09:55:20 +0530

    Update l002_String.java

--- l002_String.java
+++ l002_String.java
@@ -192,6 +192,79 @@ public class l002_String{
             return dp[n][m]=Math.min(Math.min(insert+cost[0],delete+cost[1]),replace+cost[2])+1;
     }
 
+    //edit distance 
+    public int minDistance(String s, String t, int N, int M, int[][] dp){
+        for(int n=0;n<=N;n++){
+            for(int m=0;m<=M;m++){  
+                if(n==0||m==0) {
+                    dp[n][m]=n==0?m:n;
+                    continue;
+                }
+                int insert = dp[n][m-1];//minDistance(s,t,n,m-1,dp);
+                int delete = dp[n-1][m];//minDistance(s,t,n-1,m,dp);
+                int replace = dp[n-1][m-1];//minDistance(s,t,n-1,m-1,dp);
+                
+                
+                if(s.charAt(n-1)==t.charAt(m-1)) {
+                    dp[n][m]=replace;
+                    continue;
+                }
+        
+                dp[n][m] = Math.min(insert,Math.min(delete,replace))+1;
+            }
+        }
+        return dp[N][M];
+    }
+    // wildcard
+    public String compressString(String s){
+        if (s.length() == 0)
+            return "";
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.charAt(0));
+
+        int i = 1;
+        while (i < s.length()) {
+            while (i < s.length() && sb.charAt(sb.length() - 1) == '*' && s.charAt(i) == '*')
+                i++;
+            if (i < s.length())
+                sb.append(s.charAt(i));
+            i++;
+        }
+
+        return sb.toString();
+    }
+    public int isMatch(String s, String p, int n, int m, int[][] dp){
+        if(n==0||m==0){
+            if(n==0&&m==0) return dp[n][m]=1;
+            else if(m==1&&p.charAt(m-1)=='*') return dp[n][m]=1;
+            else return dp[n][m]=0;
+        }
+        
+        if(dp[n][m]!=-1) return dp[n][m];
+        
+        char ch1=s.charAt(n-1);
+        char ch2=p.charAt(m-1);
+        
+        if(ch1==ch2||ch2=='?') return dp[n][m] = isMatch(s,p,n-1,m-1,dp);
+        else if(ch2=='*'){
+            boolean res= false;
+            res=res||(isMatch(s,p,n-1,m,dp)==1);
+            res=res||(isMatch(s,p,n,m-1,dp)==1);
+            
+            return dp[n][m]=res?1:0;
+        }
+        else return dp[n][m]=0;
+    }
+    public boolean isMatch(String s, String p) {
+        p=compressString(p);
+        System.out.println(p);
+        int n=s.length();
+        int m=p.length();
+        int[][] dp=new int[n+1][m+1];
+        
+        for(int[] d:dp) Arrays.fill(d,-1);
+        return isMatch(s,p,n,m,dp)==1?true:false;
+    }
     // count palindromic subsequence gfg
     int mod=(int)1e9+7;
     long countPS(String str, int i, int j, long[][] dp)
