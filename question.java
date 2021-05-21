a0e31915ff7184995b54610178d5e4882b9ee6e5
prachi2399 <prachi2399@gmail.com>
2021-05-21 11:40:15 +0530

    Update question.java

--- question.java
+++ question.java
@@ -611,4 +611,364 @@ public class question{
         return dp[0];
     }
 
+    // binomial coeffient memo
+    static int mod = (int)1e9+7;
+    static int nCr(int n, int k, int[][] dp){
+        if(n<k) return dp[n][k]=0;
+        if(k==0||n==k) return dp[n][k]=1;
+        if(dp[n][k]!=-1) return dp[n][k];
+        return dp[n][k]=(nCr(n-1,k-1,dp)%mod+nCr(n-1,k,dp)%mod)%mod;
+    }
+    
+    static int nCr(int n, int k){
+        int[][] dp = new int[n+1][k+1];
+        for(int[] d:dp) Arrays.fill(d,-1);
+        return nCr(n,k,dp);
+    }
+
+    // binomial coeff dp
+    static int mod = (int)1e9+7;
+    static int nCr(int N, int K, int[][] dp){
+        for(int n=0;n<=N;n++){
+            for(int k=0;k<=K;k++){
+                if(n<k)  {
+                    dp[n][k]=0;
+                    continue;
+                }
+                if(k==0||n==k) {
+                    dp[n][k]=1;
+                    continue;
+                }
+             dp[n][k]=(dp[n-1][k-1]%mod+dp[n-1][k]%mod)%mod;
+            }
+        }
+        return dp[N][K];
+    }
+    
+    static int nCr(int n, int k){
+        int[][] dp = new int[n+1][k+1];
+        //for(int[] d:dp) Arrays.fill(d,-1);
+        return nCr(n,k,dp);
+    }
+
+    // permutation coeffient memo
+    static int mod = (int)1e9+7;
+    static int nPr(int n, int k, int[][] dp){
+        if(k==0) return dp[n][k]=1;
+        if(dp[n][k]!=-1) return dp[n][k];
+        return dp[n][k]=(k*nPr(n-1,k-1,dp)%mod+nPr(n-1,k,dp)%mod)%mod;
+    }
+    
+    static int nPr(int n, int k){
+        int[][] dp = new int[n+1][k+1];
+        for(int[] d:dp) Arrays.fill(d,-1);
+        return nPr(n,k,dp);
+    }
+
+    // permutation coeff dp
+    static int mod = (int)1e9+7;
+    static int nPr(int N, int K, int[][] dp){
+        for(int n=0;n<=N;n++){
+            for(int k=0;k<=K;k++){
+                if(k==0) {
+                    dp[n][k]=1;
+                    continue;
+                }
+             dp[n][k]=(k*dp[n-1][k-1]%mod+dp[n-1][k]%mod)%mod;
+            }
+        }
+        return dp[N][K];
+    }
+    
+    static int nPr(int n, int k){
+        int[][] dp = new int[n+1][k+1];
+        //for(int[] d:dp) Arrays.fill(d,-1);
+        return nPr(n,k,dp);
+    }
+
+    ///count of balance tree of height h
+    public int balanceTree(int h){
+        if(h==0||h==1) return 1;
+
+        return balanceTree(h-1)*(2*balanceTree(h-2)+balanceTree(h-1));
+    }
+
+    public int balanceTreeDP(int h){
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+
+        for(int h=2;h=H;h++){
+            dp[h]+=dp[h-1]*(2dp[h-2]+dp[h-1]);
+        }
+        return dp[H]; 
+    }
+
+    // Count all subsequences having product less than K
+    public static int countSubsequence(int[] arr, int k) {
+        int n= arr.length;
+        int[][] arr = new int[k+1][n+1];
+
+        for(int i=1;i<=k;i++){
+            for(int j=1;j<=n;j++){
+                dp[i][j]=dp[i][j-1];
+
+                if(arr[j-1]>=0&&arr[j-1]<=i){
+                    dp[i][j]+=dp[i/arr[j-1]]+1;
+                }
+            }
+        }
+        return dp[n][k];
+    }
+
+    // Maximum sum of pairs with specific difference
+    public static int maxSumPairWithDifferenceLessThanK(int arr[], int N, int K) 
+    {
+        // Your code goes here 
+        Arrays.sort(arr);
+        int[] dp = new int[N];
+        for(int i=1;i<N;i++){
+            dp[i]=dp[i-1];
+            if(arr[i]-arr[i-1]<K){
+                if(i>=2) dp[i]=Math.max(dp[i],dp[i-2]+arr[i]+arr[i-1]);
+            
+                else dp[i]=Math.max(dp[i],arr[i]+arr[i-1]);
+            }
+        }
+        return dp[N-1];
+    }
+
+    //
+    int minCost=-1;
+    public int knapSack(int n, int W, int val[], int wt[])
+    {
+        int[][] dp = new int[n+1][W+1];
+        // for (int i = 0; i <= W; i++)
+        //     min_cost[0][i] = Integer.MAX_VALUE;
+      
+        // // fill 0'th column with 0
+        
+        for(int[] d:dp) Arrays.fill(d,(int)1e8);
+        for (int i = 1; i <= n; i++)
+            dp[i][0] = 0;
+            
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=W;j++){
+                if (wt[i-1] > j)
+                    dp[i][j] = dp[i-1][j];
+      
+                else
+                    dp[i][j] = Math.min(dp[i-1][j],dp[i][j-wt[i-1]] + val[i-1]);
+            }
+        }
+        return dp[n][W]!=(int)1e8?dp[n][W]:-1;
+    }
+    
+	public int minimumCost(int cost[], int N,int W)
+	{
+
+		int count=0;
+		for(int ele:cost) {
+		    if(ele==-1) count++;
+		}
+		int n = N-count;
+		int[] Wt= new int[N-count];
+		int[] val=new int[N-count];
+		int k=0;
+		for(int i=0;i<N;i++){
+		    if(cost[i]==-1) continue;
+		    else {
+		        val[k]=cost[i];
+		        Wt[k]=i+1;
+		        k++;
+		    }
+		}
+		
+		return knapSack(n,W,val,Wt);
+	}
+
+    // largest independent set gfg
+    public static class TreeNode{
+        int data=0,liss=0;
+        TreeNode left=null;
+        TreeNode right=null;
+
+        TreeNode(int data, int liss, TreeNode left, TreeNode right){
+            this.data=data;
+            this.liss=liss;
+            this.left=left;
+            this.right=right;
+        }
+    }
+    public static int Lis(TreeNode root) {
+        if(root==null) return 0;
+        if(root.left==null&&root.right==null) return root.liss=1;
+        if(root.liss!=0) return root.liss;
+
+        int root_excl=Lis(root.left)+Lis(root.right);
+
+        int root_incl=1;
+        if(root.left!=null){
+            root_incl+=Lis(root.left.left)+Lis(root.left.right);
+        }
+        if(root.right!=null){
+            root_incl+=Lis(root.right.left)+Lis(root.right.right);
+        }
+        return Math.max(root_excl,root_incl);
+    }
+
+    // minimum removal to make max-min<=k
+    public static int minRemoval(int[] arr, int k) {
+        int n = arr.length;
+        int[][] dp = new int[n][n];
+        for(int[] d:dp) Arrays.fill(dp,-1);
+        Arrays.sort(arr);
+        return minRemoval(arr,k,0,n-1,dp);
+    }
+    public static int minRemoval(int[] arr, int i, int j, int[][] dp) {
+        if(i>=j) return 0;
+        if(arr[i]-arr[j]<=K) return dp[i][j]=0;
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        else dp[i][j]=Math.min(minRemoval(arr,i+1,j,dp),minRemoval(arr,i,j-1,dp))+1;
+        return dp[i][j];
+    }
+    // longest common substring
+    public static int LongestCommonSubstring(String s1, String s2){
+		//write your code here
+		int[][] dp = new int[s1.length()+1][s2.length()+1];
+		int maxLen=0;
+		for(int i=1;i<=s1.length();i++){
+		    for(int j=1;j<=s2.length();j++){
+		        if(s1.charAt(i-1)==s2.charAt(j-1)){
+		            dp[i][j]=dp[i-1][j-1]+1;
+		        }
+		        maxLen=Math.max(maxLen,dp[i][j]);
+		    }
+		}
+		return maxLen;
+	}
+
+    // Reach a given score 
+    public long count(int n) {
+        long[] dp = new long[(int)n+1];
+        Arrays.fill(dp, 0);
+        dp[0] = 1;
+        
+        // Add your code here.
+        for(int i=3;i<=n;i++){
+            dp[i]+=dp[i-3];
+        }
+        for(int i=5;i<=n;i++){
+            dp[i]+=dp[i-5];
+        }
+        for(int i=10;i<=n;i++){
+            dp[i]+=dp[i-10];
+        }
+        return dp[n];
+    }
+    // 646. Maximum Length of Pair Chain
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs,(a,b)->{
+            return a[0]-b[0];
+        });
+          
+          int n = pairs.length;
+          int[] dp = new int[n];
+          int maxLen=1;
+          for(int i=0;i<n;i++){
+              dp[i]=1;
+              for(int j=i-1;j>=0;j--){
+                  if(pairs[i][0]>pairs[j][1]){
+                      dp[i]=Math.max(dp[i],dp[j]+1);
+                  }
+                  maxLen=Math.max(dp[i],maxLen);
+              }
+          }
+          return maxLen;
+    }
+    //longest alernating sequence
+    public int AlternatingaMaxLength(int[] arr)
+    {
+        // code here
+        int n = arr.length;
+        //O(n^2)
+        // int[][] dp = new int[n][2];
+        // int maxLen=1;
+        // for(int i=0;i<n;i++){
+        //     dp[i][0]=dp[i][1]=1;
+        //     for(int j=i-1;j>=0;j--){
+        //         if(arr[j]<arr[i]&&dp[i][0]<dp[j][1]+1){
+        //             dp[i][0]=dp[j][1]+1;
+        //             maxLen=Math.max(dp[i][0],maxLen);
+        //         }else if(arr[j]>arr[i]&&dp[i][1]<dp[j][0]+1){
+        //             dp[i][1]=dp[j][0]+1;
+        //             maxLen=Math.max(dp[i][1],maxLen);
+        //         }
+                
+        //     }
+        // }
+        // return maxLen;
+
+        //O(1)
+        int inc=1,dec=1;
+        for(int i=1;i<n;i++){
+            if(arr[i]>arr[i-1]){
+                inc=dec+1;
+            }else if(arr[i]<arr[i-1]){
+                dec=inc+1;
+            }
+        }
+        return  Math.max(inc,dec);
+    }
+
+    // ugly numbers2
+    public int nthUglyNumber(int n) {
+        int uglyNo=1;
+        int m_2=2,m_3=3,m_5=5;
+        int i_2=0,i_3=0,i_5=0;
+        int[] dp = new int[n];
+        for(int i=1;i<n;i++){
+            uglyNo=Math.min(m_2,Math.min(m_3,m_5));
+            dp[i]=uglyNo;
+            if(uglyNo==m_2){
+                i_2++;
+                m_2=dp[i_2]*2;
+            }
+            if(uglyNo==m_3){
+                i_3++;
+                m_3=dp[i_3]*3;
+            }
+            if(uglyNo==m_5){
+                i_5++;
+                m_5=dp[i_5]*5;
+            }
+        }
+        return uglyNo;
+    }
+    // form palindrome
+    public static int minCharsAdded(String s){
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        int len=0;
+        for(int gap=1;gap<n;gap++){
+            for(int i=0,j=gap;j<n;i++,j++){
+               if(s.charAt(i)!=s.charAt(j)) dp[i][j]=Math.min(dp[i][j-1],dp[i+1][j])+1;
+               else if(s.charAt(i)==s.charAt(j)){
+                   dp[i][j]=dp[i+1][j-1];
+               }
+            }
+        }
+        return dp[0][n-1];
+    }
+    static int countMin(String str)
+    {
+        // code here
+        int n=str.length();
+        return minCharsAdded(str);
+        
+    }
+    
 }
+
+
