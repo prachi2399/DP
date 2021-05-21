3436e3a848b4f8df95f2537cc0287a8e59bd8846
prachi2399 <prachi2399@gmail.com>
2021-05-21 11:40:29 +0530

    Update l004_TargetSum.java

--- l004_TargetSum.java
+++ l004_TargetSum.java
@@ -264,7 +264,115 @@ public class l004_TargetSum{
     }  
     
     // 494 leetcode
+
+    public int findTargetSumWays(int[] nums, int target, int n, int sum){
+        if(n==0) return target==0?1:0;
+        
+        int count = 0;
+        if(target-nums[n-1]>=0) count+=findTargetSumWays(nums,target-nums[n-1],n-1,sum);
+        if(target+nums[n-1]<=sum) count+=findTargetSumWays(nums,target+nums[n-1],n-1,sum);
+        
+        return count;
+        
+    }
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum=0;
+        for(int ele:nums) sum+=ele;
+        if(sum<target||target>-sum) return 0;
+        
+        int ans = findTargetSumWays(nums,target,nums.length,sum);
+        return ans;
+    }
+    // using 2-Ddp
+    public int findTargetSumWays(int[] nums, int target, int n, int sum,int[][] dp){
+        if(n==0) return dp[n][sum]=target==sum?1:0;
+        
+        if(dp[n][sum]==-1) return dp[n][sum];
+        int count = 0;
+        if(target-nums[n-1]>=0) count+=findTargetSumWays(nums,target,n-1,sum+nums[n-1],dp);
+        if(target+nums[n-1]<=sum) count+=findTargetSumWays(nums,target,n-1,sum-nums[n-1],dp);
+        
+        return dp[n][sum]=count;
+        
+    }
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum=0;
+        int n = nums.length;
+        for(int ele:nums) sum+=ele;
+        if(sum<target||target>-sum) return 0;
+        int[][] dp = new int[n+1][2*sum+1];
+        for(int[] d:dp) Arrays.fill(dp,-1);
+        int ans = findTargetSumWays(nums,sum+target,n,sum,dp);
+        return ans;
+    }
     
+
+    // 01 knapsack
+    int knapSack(int W, int wt[], int val[], int n, int[][] dp) 
+    { 
+       // Your code here
+       if(n==0||W==0){
+           return dp[n][W]=0;
+       }
+       if(dp[n][W]!=-1) return dp[n][W];
+       int maxCost=0;
+       if(W-wt[n-1]>=0) maxCost=knapSack(W-wt[n-1],wt,val,n-1,dp)+val[n-1];
+    
+        maxCost=Math.max(maxCost,knapSack(W,wt,val,n-1,dp));
+        return dp[n][W]=maxCost;
+    }
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       int[][] dp = new int[n+1][W+1];
+       
+       for(int[] d:dp) Arrays.fill(d,-1);
+       return knapSack(W,wt,val,n,dp);
+    } 
+    // dp
+    int knapSack(int Wt, int wt[], int val[], int N, int[][] dp) 
+    { 
+       // Your code here
+       for(int n=1;n<=N;n++){
+           for(int W=1;W<=Wt;W++){
+            //   if(n==0||W==0){
+            //         dp[n][W]=0;
+            //       continue;
+            //     }
+       int maxCost=0;
+       if(W-wt[n-1]>=0) dp[n][W]=dp[n-1][W-wt[n-1]]+val[n-1];
+       dp[n][W]=Math.max(dp[n][W],dp[n-1][W]);
+           }
+       }
+       return dp[N][Wt];
+    }
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       int[][] dp = new int[n+1][W+1];
+       
+       //for(int[] d:dp) Arrays.fill(d,-1);
+       return knapSack(W,wt,val,n,dp);
+    } 
+    
+
+    // knapsack unbounded
+    static int knapSack(int n, int W, int val[], int wt[])
+    {
+        // code here
+        int[] dp = new int[W+1];
+        for(int i=0;i<n;i++){
+            for(int j=wt[i];j<=W;j++){
+            
+                dp[j]=Math.max(dp[j],dp[j-wt[i]]+val[i]);
+            
+            }
+        }
+        return dp[W];
+    }
+
+
+
     public static void main(String[] args){
         //coinChnage();
         //numberOfSolution();
